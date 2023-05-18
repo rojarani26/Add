e4e02b8d449e7f3e5c9cef9ce9bc43395abd2e5e
M Roja Rani <88879816+rojarani26@users.noreply.github.com>
2023-05-18 16:22:13 +0530

    Fourth commit

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ li[2].style.backgroundColor = 'green';
 for(var i=0;i<li.length;i++){
     li[i].style.fontWeight = 'bold';
 }
+
+var li1 = document.getElementsByClassName('group-item');
+li1[0].style.backgroundColor = 'black';
+li1[0].style.color = 'white';
+li1[0].style.fontSize = '30px';
+li1[0].style.fontWeight = 'bold';
